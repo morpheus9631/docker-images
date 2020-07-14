3ea0a7574a120f032aa63dce1822267cd52060b3
morpheus9631 <morpheus.hsieh@gmail.com>
2020-07-14 21:33:35 +0800

    Update run.sh

--- cuda/10.1/cudnn7/run.sh
+++ cuda/10.1/cudnn7/run.sh
@@ -6,12 +6,12 @@ if [ ! -d $WORK_DIR ]; then
   chmod 775 ./$WORK_DIR
 fi
 
-cmd_str="docker run --rm -d\
+cmd="docker run --rm -d\
  --gpus all\
  -p 2022:22\
  -v $(pwd)/work:/home/$USER/$WORK_DIR\
- --name test-cuda10.1-cudnn7-devel\
- test/cuda:10.1-cudnn7-devel"
+ --name cuda10.1-cudnn7-devel\
+ morpheus9631/cuda:10.1-cudnn7-devel"
 
-echo $cmd_str
-eval $cmd_str
+echo $cmd
+eval $cmd
