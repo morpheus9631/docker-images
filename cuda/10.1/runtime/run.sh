501c19ddd30a3d9ade2c8022ddcbbd639f638324
morpheus9631 <morpheus.hsieh@gmail.com>
2020-07-14 21:31:15 +0800

    Update run.sh

--- cuda/10.1/runtime/run.sh
+++ cuda/10.1/runtime/run.sh
@@ -6,12 +6,12 @@ if [ ! -d $WORK_DIR ]; then
   chmod 775 ./$WORK_DIR
 fi
 
-cmd_str="docker run --rm -d\
+cmd="docker run --rm -d\
  --gpus all\
  -p 2022:22\
  -v $(pwd)/work:/home/$USER/$WORK_DIR\
- --name test-cuda10.1-runtime\
- test/cuda:10.1-runtime"
+ --name cuda10.1-runtime\
+ morpheus9631/cuda:10.1-runtime"
 
-echo $cmd_str
-eval $cmd_str
+echo $cmd
+eval $cmd
