582cba0b656700e2a78c69ff93b5e83626f4fae1
morpheus9631 <morpheus.hsieh@gmail.com>
2020-07-15 08:47:40 +0800

    Update run.sh

--- cuda/10.2/base/run.sh
+++ cuda/10.2/base/run.sh
@@ -10,8 +10,8 @@ cmd="docker run --rm -d\
  --gpus all\
  -p 2022:22\
  -v $(pwd)/work:/home/user/$WORK_DIR\
- --name test-cuda10.2\
- test/cuda:10.2-base"
+ --name cuda10.2\
+ morpheus9631/cuda:10.2-base"
 
 echo $cmd
 eval $cmd
